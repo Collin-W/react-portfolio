dea17513037b7e8c9a77a4492c2e5a04897efcf6
Collin-W <whalencollin@gmail.com>
2022-01-07 13:11:09 -0600

    created the contact form

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'; //{ useState }
+import Header from './components/Header/index'
+import Footer from './components/Footer/index'
+import Nav from './components/Navigation/index'
+import Projects from './components/Projects/index'
+
+function App() {
+
+
+
+
+
+return (
+    <div>
+        
+    </div>
+)
+
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <section className="header">
+
+            <h1 className="my-name">Collin Whalen</h1>
+
+            <h3>About Me</h3>
+            <h3>Projects</h3>
+            <h3>Contact Me</h3>
+            <h3>Resume</h3>
+
+        </section>
+    )
+}
+
+export default Header;
